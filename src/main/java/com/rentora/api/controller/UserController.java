587ebde80683afe9c7c8149667da2e92fd18a890
octovir <octovir@gmail.com>
2025-10-15 04:03:23 +0700

    feat : add update user service in AuthController

--- src/main/java/com/rentora/api/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.rentora.api.controller;
-
-public class UserController {
-}
--- src/main/java/com/rentora/api/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.rentora.api.service;
-
-import com.rentora.api.repository.UserRepository;
-
-public class UserService {
-
-}
